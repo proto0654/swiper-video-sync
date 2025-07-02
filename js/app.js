99e32091524ef8769f6c5764aa57cc9c22b7b740
Your Name <your-email@example.com>
2025-07-02 23:18:52 +0300

    Initial commit: Background animation website

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 const video = document.querySelector(".video-background");
 
 const swiperText = new Swiper(".swiper", {
-  loop: true,
   speed: 1000,
   mousewheel: {},
   pagination: {
@@ -13,44 +12,12 @@ const swiperText = new Swiper(".swiper", {
     nextEl: ".swiper-button-next",
   },
 });
-
-function handleVideoPlayback(swiper, direction) {
-  gsap.killTweensOf(video);
-  const newTime = (swiper.realIndex % 2) * (video.duration / 2);
-
-  if (direction === "next") {
-    // Forward playback
-    if (newTime < video.currentTime) {
-      // This handles the loop: animate to the end, then jump to the start
-      gsap.to(video, 1, {
-        currentTime: video.duration,
-        ease: "power2.in",
-        onComplete: () => {
-          video.currentTime = 0; // Jump to start after reaching the visual end
-        },
-      });
-    } else {
-      gsap.to(video, 1, { currentTime: newTime, ease: "power2.in" });
-    }
-  } else {
-    // direction === 'prev'
-    // Backward playback
-    if (newTime > video.currentTime) {
-      // This handles the loop: jump to the end to rewind from there
-      video.currentTime = video.duration;
-    }
-    gsap.to(video, 1, { currentTime: newTime, ease: "power2.in" });
-  }
-}
-
-swiperText.on("slideNextTransitionStart", function () {
-  handleVideoPlayback(this, "next");
+swiperText.on("slideChange", function () {
+  gsap.to(video, 1, {
+    currentTime: (video.duration / (this.slides.length - 1)) * this.realIndex,
+    ease: "power1.in",
+  });
 });
-
-swiperText.on("slidePrevTransitionStart", function () {
-  handleVideoPlayback(this, "prev");
-});
-
 swiperText
   .on("slideChangeTransitionStart", function () {
     video.classList.add("change");
